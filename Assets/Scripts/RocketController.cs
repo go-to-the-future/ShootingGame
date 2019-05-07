812cf86739cc47888133c3c83d03bec821a6af61
Kiyotan <appleumasugi.mjd@gmail.com>
2019-05-07 16:28:46 +0900

    First-commit

---
+++ Assets/Scripts/RocketController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RocketController : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-0.1f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(0.1f, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
